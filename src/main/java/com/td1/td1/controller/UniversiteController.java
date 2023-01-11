732958780a2b59fa65b304e1351f7ffe6b585011
souhayel <souhayel.esprint@gmail.com>
2023-01-11 16:06:03 +0100

    Fix - update requestMapping for the Universite controller to be Universites

--- src/main/java/com/td1/td1/controller/UniversiteController.java
+++ src/main/java/com/td1/td1/controller/UniversiteController.java
@@ -18,7 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Universite")
 @RestController
-@RequestMapping("/api/v1/universite")
+@RequestMapping("/api/v1/universites")
 public class UniversiteController {
     @Autowired
     UniversiteService universiteService;
